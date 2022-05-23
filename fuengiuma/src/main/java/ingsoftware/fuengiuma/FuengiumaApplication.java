cf98ac7c3c3e108e91858b38101c33c06d3b04d6
javidfdez <javierdiaz6702@gmail.com>
2022-05-23 14:11:47 +0200

    Creacion del proyecto base

---
+++ fuengiuma/src/main/java/ingsoftware/fuengiuma/FuengiumaApplication.java
@@ -0,0 +1,13 @@
+package ingsoftware.fuengiuma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuengiumaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuengiumaApplication.class, args);
+	}
+
+}
---
+++ fuengiuma/src/test/java/ingsoftware/fuengiuma/FuengiumaApplicationTests.java
@@ -0,0 +1,13 @@
+package ingsoftware.fuengiuma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuengiumaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
